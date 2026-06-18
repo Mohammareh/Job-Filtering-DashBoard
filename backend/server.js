2f9b503005d6489f06841a1b93e9f445b8b9447a
Mohammad Alrassas <xdmohammad83@gmail.com>
2026-06-18 11:44:53 +0300

    More setup for the backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  console.log("Here");
+  res.render("index", { text1: "WOrld" });
+});
+
+app.get("/users", (req, res) => {
+  res.send("User List");
+});
+
+app.get("/users/new", (req, res) => {
+  res.send("User New Form");
+});
+
+app.listen(3000);
